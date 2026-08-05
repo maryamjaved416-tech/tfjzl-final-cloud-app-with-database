90fafdc53f230867a595262ee622bfdabc2567d6
maryamjaved416-tech <maryamjaved416@gmail.com>
2026-08-05 23:56:35 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name = 'coursequiz'
 
 urlpatterns = [
-    # baaki ke paths...
+    # baaki paths...
     
+    # 1. submit path
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    path('<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
+    
+    # 2. show_exam_result path  
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 ]
